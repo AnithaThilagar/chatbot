20aced3ac30b4459416b83b3ced6798dd1f96d02
Anitha T <39959@hexaware.com>
2018-01-30 12:09:42 +0530

    Code commit for chatbot

--- webhook.js
+++ webhook.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const request = require('request');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -31,3 +32,24 @@ app.post('/webhook', (req, res) => {
     res.status(200).end();
   }
 });
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: 'EAAdg47VZC9lIBAL58o4aCScmw6yi2gq3z6GUDUruH56ZAVEtTXmo2e3sa3ct3IPFxJvYpmZBUVD04nBCypVxhbgMLxoddVkjoXd6ZCDKw3fvVGs4txZBZAU0rB5xER3CMDSrsWwWyONL9aJORuagox8qsZBdDkS1FCWzHGNSYU9QwZDZD'},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
