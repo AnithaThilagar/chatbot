0cc7d085bd55f76999d14bb0314539448cc3e764
Anitha T <39959@hexaware.com>
2018-01-31 14:30:23 +0530

    Code commit to get the weather

--- webhook.js
+++ webhook.js
@@ -1,18 +1,26 @@
+'use strict';
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const request = require('request');
-app.use(bodyParser.urlencoded({ extended: true }));
+const apiai = require('apiai');
+
+const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(process.env.PORT || 2000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
+const apiaiApp = apiai(APIAI_TOKEN);
+
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
-  console.log('Inside get method');
-  if (req.query['hub.verify_token'] === 'chatBot') {//req.query['hub.mode'] && 
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'chatBot') {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).end();
@@ -21,9 +29,8 @@ app.get('/webhook', (req, res) => {
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
-  console.log('Inside post method');
-	console.log(req.body.object);
-  /*if (req.body.object === 'page') {
+  console.log(req.body);
+  if (req.body.object === 'page') {
     req.body.entry.forEach((entry) => {
       entry.messaging.forEach((event) => {
         if (event.message && event.message.text) {
@@ -32,31 +39,83 @@ app.post('/webhook', (req, res) => {
       });
     });
     res.status(200).end();
-  }*/
+  }
 });
 
+
+/* GET query from API.ai */
+
 function sendMessage(event) {
-  console.log("Inside send events ");
   let sender = event.sender.id;
   let text = event.message.text;
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/2001976020018830',
-	qs: {access_token: 'EAAdg47VZC9lIBAGbKwIqVtdYuNNDrjl0BiPis2mJGXXAMXmX8x7U0eUhkPOCUQ1VRriXLflZAUGHBK2ZCWJMvm6U69lH8N6ZA1YKiva4lBymbdMOBwUZCO7MDvimvozZCJQe6uTXGTy1V5nCZAYbZCmIAYO6A4oo6pfZChZCX19vf0AAZDZD'},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'tabby_cat'
   });
-  console.log("Req "+request);
+
+  apiai.on('response', (response) => {
+    console.log(response)
+    let aiText = response.result.fulfillment.speech;
+
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: 'EAAdg47VZC9lIBAGbKwIqVtdYuNNDrjl0BiPis2mJGXXAMXmX8x7U0eUhkPOCUQ1VRriXLflZAUGHBK2ZCWJMvm6U69lH8N6ZA1YKiva4lBymbdMOBwUZCO7MDvimvozZCJQe6uTXGTy1V5nCZAYbZCmIAYO6A4oo6pfZChZCX19vf0AAZDZD'},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: aiText}
+      }
+    }, (error, response) => {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+  });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
 }
+
+/* Webhook for API.ai to get response from the 3rd party API */
+app.post('/ai', (req, res) => {
+  console.log('*** Webhook for api.ai query ***');
+  console.log(req.body.result);
+
+  if (req.body.result.action === 'weather') {
+    console.log('*** weather ***');
+    let city = req.body.result.parameters['geo-city'];
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+'818cea3bd65bf4ace834a24d7adca117'+'&q='+city;
+
+    request.get(restUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let json = JSON.parse(body);
+        console.log(json);
+        let tempF = ~~(json.main.temp * 9/5 - 459.67);
+        let tempC = ~~(json.main.temp - 273.15);
+        let msg = 'The current condition in ' + json.name + ' is ' + json.weather[0].description + ' and the temperature is ' + tempF + ' ℉ (' +tempC+ ' ℃).'
+        return res.json({
+          speech: msg,
+          displayText: msg,
+          source: 'weather'
+        });
+      } else {
+        let errorMessage = 'I failed to look up the city name.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+      }
+    })
+  }
+
+});
 
 
 //https://test-fb-chat-bot.herokuapp.com/webhook?hub.verify_token=chatBot
